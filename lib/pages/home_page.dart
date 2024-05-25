7adc9f088f0504fd459a8cfe6c9956445c8f0157
andre <d3deco54@gmail.com>
2024-05-25 16:16:38 -0300

    Ajustes na página do pokémon

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_pokedex/breakpoints.dart';
 
 import 'pokemon_page.dart';
 
@@ -24,138 +25,141 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 96,
+        toolbarHeight: MediaQuery.of(context).size.width > breakpointMobile ? 96 : 72,
         backgroundColor: const Color.fromRGBO(49, 102, 176, 1),
         centerTitle: true,
-        title: const Text(
+        title: Text(
           'Pokédex',
           style: TextStyle(
-            fontSize: 64,
+            fontSize: MediaQuery.of(context).size.width > breakpointMobile ? 64 : 48,
             fontWeight: FontWeight.bold,
-            color: Color.fromRGBO(255, 204, 1, 1),
+            color: const Color.fromRGBO(255, 204, 1, 1),
           ),
         ),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              alignment: Alignment.center,
-              width: MediaQuery.of(context).size.width * 0.8,
-              height: 64,
-              color: Colors.grey[200],
-              child: Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintStyle: TextStyle(
-                      color: Colors.grey[500],
+        child: SizedBox(
+          width: breakpointMobile,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                alignment: Alignment.center,
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: 64,
+                color: Colors.grey[200],
+                child: Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintStyle: TextStyle(
+                        color: Colors.grey[500],
+                        fontSize: 24,
+                      ),
+                      hintText: 'Search pokémon',
+                      border: const OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                      ),
+                      prefixIcon: const Icon(Icons.search),
+                    ),
+                    style: const TextStyle(
+                      color: Colors.black,
                       fontSize: 24,
                     ),
-                    hintText: 'Search pokémon',
-                    border: const OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                    ),
-                    prefixIcon: const Icon(Icons.search),
-                  ),
-                  style: const TextStyle(
-                    color: Colors.black,
-                    fontSize: 24,
-                  ),
-                  controller: textController,
-                  onSubmitted: (value) async {
-                    if (value.isNotEmpty) {
-                      try {
-                        final response = await dio.get(
-                            'https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}'
-                        );
-                        List<String> types = [];
-                        for(var type in response.data['types']){
-                          types.add(type['type']['name']);
+                    controller: textController,
+                    onSubmitted: (value) async {
+                      if (value.isNotEmpty) {
+                        try {
+                          final response = await dio.get(
+                              'https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}'
+                          );
+                          List<String> types = [];
+                          for(var type in response.data['types']){
+                            types.add(type['type']['name']);
+                          }
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => PokemonPage(
+                                name: response.data['name'],
+                                id: response.data['id'].toString(),
+                                spriteUrl: response.data['sprites']['front_default'],
+                                types: types,
+                              ),
+                            )
+                          );
+                        } on DioException catch (e) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                                content: Text('Pokémon not found. Error status: ${e.response?.statusCode}'),
+                                backgroundColor: Colors.red,
+                                action: SnackBarAction(
+                                  label: 'Dismiss',
+                                  onPressed: () {
+                                    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                                  },
+                                )
+                            )
+                          );
                         }
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) => PokemonPage(
-                              name: response.data['name'],
-                              id: response.data['id'].toString(),
-                              spriteUrl: response.data['sprites']['front_default'],
-                              types: types,
-                            ),
-                          )
-                        );
-                      } on DioException catch (e) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                              content: Text('Pokémon not found. Error status: ${e.response?.statusCode}'),
-                              backgroundColor: Colors.red,
-                              action: SnackBarAction(
-                                label: 'Dismiss',
-                                onPressed: () {
-                                  ScaffoldMessenger.of(context).hideCurrentSnackBar();
-                                },
-                              )
-                          )
-                        );
                       }
                     }
-                  }
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 32),
-            ElevatedButton(
-              onPressed: () async {
-                var value = textController.text;
-                if (value.isNotEmpty) {
-                  try {
-                    final response = await dio.get(
-                        'https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}'
-                    );
-                    List<String> types = [];
-                    for(var type in response.data['types']){
-                      types.add(type['type']['name']);
-                    }
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => PokemonPage(
-                          name: response.data['name'],
-                          id: response.data['id'].toString(),
-                          spriteUrl: response.data['sprites']['front_default'],
-                          types: types,
-                        ),
-                      )
-                    );
-                  } on DioException catch (e) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text('Pokémon not found. Error status: ${e.response?.statusCode}'),
-                        backgroundColor: Colors.red,
-                        action: SnackBarAction(
-                          label: 'Dismiss',
-                          onPressed: () {
-                            ScaffoldMessenger.of(context).hideCurrentSnackBar();
-                          },
+              const SizedBox(height: 32),
+              ElevatedButton(
+                onPressed: () async {
+                  var value = textController.text;
+                  if (value.isNotEmpty) {
+                    try {
+                      final response = await dio.get(
+                          'https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}'
+                      );
+                      List<String> types = [];
+                      for(var type in response.data['types']){
+                        types.add(type['type']['name']);
+                      }
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => PokemonPage(
+                            name: response.data['name'],
+                            id: response.data['id'].toString(),
+                            spriteUrl: response.data['sprites']['front_default'],
+                            types: types,
+                          ),
                         )
-                      )
-                    );
+                      );
+                    } on DioException catch (e) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Pokémon not found. Error status: ${e.response?.statusCode}'),
+                          backgroundColor: Colors.red,
+                          action: SnackBarAction(
+                            label: 'Dismiss',
+                            onPressed: () {
+                              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                            },
+                          )
+                        )
+                      );
+                    }
                   }
-                }
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color.fromRGBO(255, 204, 1, 1),
-              ),
-              child: const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                  'Find pokémon',
-                  style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.black,
-                  )
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromRGBO(255, 204, 1, 1),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'Find pokémon',
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.black,
+                    )
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         )
       )
     );
--- lib/pages/pokemon_page.dart
+++ lib/pages/pokemon_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pokedex/breakpoints.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class PokemonPage extends StatelessWidget {
   final String name;
@@ -16,35 +18,83 @@ class PokemonPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+   return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        toolbarHeight: 96,
+        toolbarHeight: MediaQuery.of(context).size.width > breakpointMobile ? 96 : 72,
         backgroundColor: const Color.fromRGBO(49, 102, 176, 1),
         centerTitle: true,
         title: Text(
           name.capitalize(),
-          style: const TextStyle(
-            fontSize: 64,
+          style: TextStyle(
+            fontSize: MediaQuery.of(context).size.width > breakpointMobile ? 64 : 48,
             fontWeight: FontWeight.bold,
-            color: Color.fromRGBO(255, 204, 1, 1),
+            color: const Color.fromRGBO(255, 204, 1, 1),
           ),
         ),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.network(
-              spriteUrl,
-              width: 300,
-              height: 300,
-              fit: BoxFit.cover,
+        child: SizedBox(
+          width: breakpointMobile,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.grey[200],
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.network(
+                          spriteUrl,
+                          width: 300,
+                          height: 300,
+                          fit: BoxFit.contain,
+                        ),
+                        Text(name.capitalize(),
+                          style: const TextStyle(
+                            fontSize: 32,
+                          ),
+                        ),
+                        Text("#$id",
+                          style: const TextStyle(
+                            fontSize: 32,
+                          ),
+                        ),
+                        Text("Types: ${types.join(", ").capitalize()}",
+                          style: const TextStyle(
+                            fontSize: 24,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 32),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromRGBO(255, 204, 1, 1),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      "Back",
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                )
+              ],
             ),
-            Text("Name: ${name.capitalize()}"),
-            Text("#$id"),
-            Text("Types: ${types.join(", ").capitalize()}"),
-          ],
+          ),
         ),
       )
     );
